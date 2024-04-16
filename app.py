d86c81f7bbdd10e08009a68f71c5b626fed24b94
Kristoffer <33570390+Kristoffermg@users.noreply.github.com>
2024-04-16 14:47:13 +0200

    API call progress

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, request, jsonify
+from flask_cors import CORS
+import requests
 
 app = Flask(__name__, template_folder='templates')
+CORS(app, origins=['http://127.0.0.1:5000'])
 
 @app.route('/')
 def index():
@@ -15,6 +18,15 @@ def endpoint():
         return query_data()
     else:
         return "Unknown item"
+
+@app.route('/insert', methods=['POST'])
+def insert_trajectory():
+    data = request.get_json()
+    
+    x = requests.post("http://localhost:8080/insert", json = data)
+
+    return jsonify(data), 200
+
 
 def insert_trajectory():
     # Here you can handle the insertion of trajectory
